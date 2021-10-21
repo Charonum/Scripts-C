362bce8fa3ac4bbbaad403fe11081c16a04ffe87
Charonum <78572976+Charonum@users.noreply.github.com>
2021-10-21 08:51:00 -0700

    Add files via upload

---
+++ ThirdPersonCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+	public Transform camRoot;
+	public Vector2 pitchMinMax = new Vector2(-40, 85);
+	public float mouseSensitivity;
+	float pitch, yaw;
+	void Start()
+	{
+		Cursor.visible = false;
+	}
+
+	void Update()
+	{
+		pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+		pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+		yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+
+		transform.eulerAngles = new Vector3(0f, yaw, 0f);
+		camRoot.eulerAngles = new Vector3(pitch, camRoot.eulerAngles.y, 0f);
+	}
+}
